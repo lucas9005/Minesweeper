39bdd2a895e98b6e81fcecd7b686fae4546b42b3
Lucas Aaron Ruggiero <lucas9005@gmail.com>
2018-09-19 01:44:11 -0300

    Updated minesweeper component test cases

--- src/app/components/minesweeper/minesweeper.component.spec.ts
+++ src/app/components/minesweeper/minesweeper.component.spec.ts
@@ -1,25 +1,68 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+// Angular
+import { TestBed, ComponentFixture, async } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
-import { MinesweeperComponent } from './minesweeper.component';
+// Components
+import { MinesweeperComponent } from '../../components/minesweeper/minesweeper.component';
+import { BoardComponent } from '../../components/board/board.component';
+import { TileComponent } from '../../components/tile/tile.component';
+import { ScoreboardComponent } from '../../components/scoreboard/scoreboard.component';
 
+// Services
+import { MinesweeperService } from '../../services/minesweeper.service';
+
+// Test
 describe('MinesweeperComponent', () => {
   let component: MinesweeperComponent;
   let fixture: ComponentFixture<MinesweeperComponent>;
 
+  // Before each run
   beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ MinesweeperComponent ]
-    })
-    .compileComponents();
+    // Configure
+    TestBed.configureTestingModule(
+      {
+        declarations: [
+          MinesweeperComponent,
+          BoardComponent,
+          TileComponent,
+          ScoreboardComponent
+        ],
+        providers: [
+          MinesweeperService
+        ]
+      }
+    ).compileComponents();
   }));
 
+  // Before each run
   beforeEach(() => {
+    // Initialize
     fixture = TestBed.createComponent(MinesweeperComponent);
     component = fixture.componentInstance;
+    // Update the component
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  // Should create the component
+  it('should create the component', () => {
+    // Assert
     expect(component).toBeTruthy();
   });
+
+  // Should show the game scoreboard
+  it('should show the game scoreboard', () => {
+    // Get the game scoreboard element
+    const gameScoreboard = fixture.debugElement.query(By.css('app-scoreboard'));
+    // Assert
+    expect(gameScoreboard).toBeTruthy();
+  });
+
+  // Should show the game board
+  it('should show the game board', () => {
+    // Get the game board element
+    const gameBoard = fixture.debugElement.query(By.css('app-board'));
+    // Assert
+    expect(gameBoard).toBeTruthy();
+  });
+
 });
